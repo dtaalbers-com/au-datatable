36be7acf64e52889ff4b8f0f840217b62395521f
Tom Aalbers <dtaalbers@gmail.com>
2017-09-29 23:08:28 +0200

    [fix] Fixed app version

---
+++ docs/src/AppConfiguration.ts
@@ -0,0 +1,3 @@
+export class AppConfiguration {
+    public static appVersion: string = 'v1.0.0-beta-002';
+}
--- docs/src/pages/intro/intro.ts
+++ docs/src/pages/intro/intro.ts
@@ -1,8 +1,11 @@
 import { Router } from 'aurelia-router';
 import { autoinject } from 'aurelia-framework';
+import { AppConfiguration } from '../../AppConfiguration';
 
 @autoinject()
 export class IntroPage {
+
+    public appVersion: string = AppConfiguration.appVersion;
 
     constructor(
         private router: Router
