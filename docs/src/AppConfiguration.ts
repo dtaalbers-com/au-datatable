2f6ef91352fb474120346718cf7e27cfebc273aa
Tom Aalbers <dtaalbers@gmail.com>
2017-10-29 13:23:42 +0100

    [update] updated plugin & docs versions

--- docs/src/AppConfiguration.ts
+++ docs/src/AppConfiguration.ts
@@ -1,4 +1,4 @@
 export class AppConfiguration {
-    public static appVersion: string = 'v1.0.0-beta-002';
-    public static docsVersion: string = 'v1.0.2-beta';
+    public static appVersion: string = 'v1.0.0-beta-003';
+    public static docsVersion: string = 'v1.0.3-beta';
 }
