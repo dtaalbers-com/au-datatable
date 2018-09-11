52aaaa61d2df4e2d3a35fca11e2838c1dbece7f1
Tom Aalbers <dtaalbers@gmail.com>
2018-09-11 18:23:21 +0200

    Added demo project

---
+++ demo/src/models/user.ts
@@ -0,0 +1,12 @@
+export interface IUser {
+    id: string;
+    name: string;
+    street: string;
+    postcode: string;
+    state: string;
+    city: string;
+    username: string;
+    thumbnail: string;
+    nat: string;
+    email: string;
+}
